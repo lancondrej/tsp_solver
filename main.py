d071ffefad243b1f15a08fe4399dbb288a422521
Ondřej Lanč <lancondr@fjfi.cvut.cz>
2016-06-19 14:27:47 +0200

    main

---
+++ main.py
@@ -0,0 +1,45 @@
+__author__ = 'Ondřej Lanč'
+
+import matplotlib.pyplot as plt
+from world.world import World
+from solver.greedy import greedy_all
+from solver.brute import brute
+from solver.held_karp import hk
+import timeit
+from solver.ACO.colony import Colony
+
+
+count_of_cities = 30
+upper_bound = 1000
+city_seed = 3
+world=World()
+world.random_world(count_of_cities, upper_bound, upper_bound, city_seed)
+
+print("greedy")
+start_time = timeit.default_timer()
+route=greedy_all(world)
+distance=route.length()
+print(distance)
+print(timeit.default_timer() - start_time)
+
+# print("brute")
+# start_time = timeit.default_timer()
+# route=brute(world)
+# distance=route.length()
+# print(distance)
+# print(timeit.default_timer() - start_time)
+
+# print("HK")
+# start_time = timeit.default_timer()
+# route=hk(world)
+# distance=route.length()
+# print(distance)
+# print(timeit.default_timer() - start_time)
+
+print("ACO")
+start_time = timeit.default_timer()
+colony = Colony(world, 5000)
+route=colony.solve()
+distance=route.length()
+print(distance)
+print(timeit.default_timer() - start_time)
